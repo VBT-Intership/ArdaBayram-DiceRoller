c64d51b9e5dde57c3caa2648ff56090c6b4e4383
Arda Bayram <56687124+arda-bayram@users.noreply.github.com>
2020-08-19 13:10:35 +0300

    Count up button and its action is added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,14 +10,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Roll button clicked", Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        Toast.makeText(this, "Count up button clicked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        var diceValue = resultText.text.toString().toIntOrNull()
+
+        if(diceValue == null){
+            diceValue = 1
+        }else if(diceValue < 6){
+            diceValue++
+        }
+
+        resultText.text = diceValue.toString()
+
     }
 }
